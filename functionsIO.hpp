8be474b57c5160a9545ef584365f5f3d9ac11b2f
brencass20 <33562885+brencass20@users.noreply.github.com>
2018-11-26 20:34:44 -0700

    Changed to vector<Operation*> *allOperations per Evan

--- functionsIO.hpp
+++ functionsIO.hpp
@@ -16,16 +16,16 @@
 using namespace std;
 
 //File Input
-vector<Variable> inputFileToVariables(string fileName, vector<Operation> *allOps);
+vector<Variable> inputFileToVariables(string fileName, vector<Operation*> *allOps);
 
 vector<Variable> compileListOfVariables(string line, vector<Variable> allVariables,	string currType, 
 	string bitWidth, string varNames, Variable tempVar, string delimiter, string currName,	int pos);
 
-void compileListOfOperations(string line, vector<Variable> allVariables, vector<Operation> *allOperations,
+void compileListOfOperations(string line, vector<Variable> allVariables, vector<Operation*> *allOperations,
 	vector<Variable> currOperand, string val, int count, bool validVar, bool signedFlag,
 	int maxDatawidth, int operandCount,	bool flagIncDec);
 
-void dependentOperation(Operation *currOperation, vector<Operation> *allOperations);
+void dependentOperation(Operation *currOperation, vector<Operation*> *allOperations);
 
 //File Output
 void outputFileCreate(vector<Variable> allVariables, string outFile);
